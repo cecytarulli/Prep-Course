6fee196a231d1deeec39f597396192e9cdaaa405
cecytarulli <cecytarulli@gmail.com>
2022-04-20 12:55:35 -0300

    tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str 
   
 }
 
@@ -35,6 +36,8 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  var (x + y);
+  return suma 
   
 }
 
