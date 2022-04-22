9e9131d7547ea44f8512b4c1746ed6eb0b81118e
cecytarulli <cecytarulli@gmail.com>
2022-04-22 12:03:36 -0300

    practica extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -18,6 +18,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {};
+  for (var i = 0; i < string.length; i++) {
+    if(objeto.hasOwnProperty(string[i])){
+      objeto[string[i]] = objeto[string[i]]
+    }
+    else{
+      objeto[string[i]] = 1
+    }
+    return objeto 
+  }
 }
 
 
@@ -26,6 +36,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var may = "";
+  var min = "";
+  for (var i = 0; i < s.length; i++) {
+    if(s[i] === s[i].toLowerCase()){
+      min += s[i]
+    }
+    else{
+      may += s[i]
+    }
+
+  }
+  return may + min 
 }
 
 
@@ -35,6 +57,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var inverso = str.split(" ").map(function (elemento){
+    return elemento.split("").reverse().join
+  })
+  return inverso.join(" ") 
+
 } 
 
 
@@ -43,13 +70,20 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numero2 = numero.toString().split("").reverse().join("")
+  if (numero.toString === numero2)
+return "Es capicua"
+} else {
+  return "No es capicua"
 }
+
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  return cadena.replace(/a/g, "" ).replace(/b/g, "").replace(/c/g, "")
 }
 
 
@@ -57,6 +91,7 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  
 }
 
 
